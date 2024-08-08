1b6ac23d0e580051e50d2ce5a6008019d88e97f0
DSikes <david@sikespc.com>
2024-08-08 12:47:46 -0400

    Modified for brevity during testing

--- src/EspBoardHelpers/SketchInitializers.h
+++ src/EspBoardHelpers/SketchInitializers.h
@@ -1,17 +1,17 @@
+// //
+// // Created by David on 5/26/2023.
+// //
 //
-// Created by David on 5/26/2023.
+// #ifndef COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
+// #define COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
 //
-
-#ifndef COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
-#define COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
-
-
-class SketchInitializers
-{
-public:
-    static void InitializeSpiPins();
-    static void InitializeRemotePins(const String &whichCar);
-};
-
-
-#endif //COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
+//
+// class SketchInitializers
+// {
+// public:
+//     static void InitializeSpiPins();
+//     static void InitializeRemotePins(const String &whichCar);
+// };
+//
+//
+// #endif //COROLLA_2003_NFC_CONTROLLER_SKETCHINITIALIZERS_H
