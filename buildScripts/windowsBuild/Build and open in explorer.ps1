c4b851b1e5d7fa88e847d6c4ba3ed7fd1c913429
DSikes <david@sikespc.com>
2024-08-15 08:06:43 -0400

    Add yes no prompt to open explorer build folder and upload page

--- buildScripts/windowsBuild/Build and open in explorer.ps1
+++ buildScripts/windowsBuild/Build and open in explorer.ps1
@@ -4,10 +4,25 @@ pio run
 
 cd .pio\build\wt32-eth01\
 
-explorer .
+Write-Host ""
+Write-Host "Build script build portion completed."
+Write-Host ""
+Write-Host "Open explorer folder with firmware bin and firefox to upload page? Press Y to open, any other key to not: " -NoNewLine
 
-echo "Drag firmware.bin onto the ElegantOTA page"
+$key = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-#explorer "http://192.168.1.243/update"
+if ($key.Character -eq 'y')
+{
+    explorer .
+
+    echo "Drag firmware.bin onto the ElegantOTA page"
+
+    ."C:\Program Files\Mozilla Firefox\firefox.exe" "http://192.168.1.50/update"
+}
 
 cd ../../../buildScripts/windowsBuild
+
+Write-Host ""
+Write-Host ""
+Write-Host "Build script execution completed."
+Write-Host ""
